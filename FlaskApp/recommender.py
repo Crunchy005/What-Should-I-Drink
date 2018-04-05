e37f7c262dabd45a440d0c8a44a4d693752a9b38
Mark <markorland@live.com>
2018-04-05 09:35:26 -0700

    flask mostly done

--- FlaskApp/recommender.py
+++ FlaskApp/recommender.py
@@ -15,52 +15,37 @@ def recommender(selection):
     recommended_list = df_target["name"].unique()[:3]
     return recommended_list
 
-
 # ------------------------------------------------------------------------------------------------------------------
 
 @app.route('/', methods=['POST', 'GET'])
 def home_page():
     return render_template('index.html')
 
-
-@app.route('/beer', methods=['POST', 'GET'])
+@app.route('/whiskey_rec', methods=['POST', 'GET'])
 def dropdown_b():
-    if request.method == 'POST':
-        recommendations = recommender(request.form.get('beer_selection'))
-        submitted = True
-    else:
-        recommendations = []
-        submitted = False
+    selected = request.form.get('beer_selection')
+    recommendations = [] if not selected else recommender(selected)
 
-    return render_template('beer.html', beers=beers_list, selected=request.form.get('beer_selection'), submitted=submitted, recommendations=recommendations)
+    return render_template(
+        'whiskey_rec.html',
+        beers=beers_list,
+        selected=selected,
+        submitted=bool(selected),
+        recommendations=recommendations
+    )
 
-
-# @app.route('/whiskey', methods=['POST', 'GET'])
-# def dropdown_w():
-#     if request.method == 'POST':
-#         recommendations = recommender(request.form.get('whiskey_selection'))
-#         submitted = True
-#     else:
-#         recommendations = []
-#         submitted = False
-
-#     return render_template('whiskey.html', whiskeys=whiskey_list, selected=request.form.get('whiskey_selection'), submitted=submitted, recommendations=recommendations)
-
-@app.route('/whiskey', methods=['POST', 'GET'])
+@app.route('/beer_rec', methods=['POST', 'GET'])
 def dropdown_w():
     selected = request.form.get('whiskey_selection')
     recommendations = [] if not selected else recommender(selected)
 
     return render_template(
-        'whiskey.html',
+        'beer_rec.html',
         whiskeys=whiskey_list,
         selected=selected,
         submitted=bool(selected),
         recommendations=recommendations
     )
 
-
-
 if __name__ == "__main__":
     app.run()
-
